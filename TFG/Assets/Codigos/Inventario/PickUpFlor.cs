66081eea26914431624cdb4af3e9012d5a8b9f7f
ivaan2212 <ivaan2212@gmail.com>
2024-05-07 18:36:31 +0200

    Cambios en los dialogos

--- TFG/Assets/Codigos/Inventario/PickUpFlor.cs
+++ TFG/Assets/Codigos/Inventario/PickUpFlor.cs
@@ -14,7 +14,6 @@ public class PickUpFlor : MonoBehaviour
     public bool Cflor;
     public GameObject Ghosty;
     //public GameObject GhostyOther;
-    public GameObject spawn;
 
     // Start is called before the first frame update
     private void Start()
@@ -22,18 +21,22 @@ public class PickUpFlor : MonoBehaviour
         inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<InventoryFlor>();
         itemPrefab.GetComponent<Button>().onClick.AddListener(RecogerObjeto);
         Ghosty = GameObject.FindGameObjectWithTag("Ghosty");
-        //GhostyOther = GameObject.FindGameObjectWithTag("GhostyFlor");    
+   
     }
 
     // Update is called once per frame
     private void Update()
     {
+
         // Verifica si se presiona la tecla para recoger el objeto
         if (Input.GetKeyDown(teclaRecoger))
         {
+
             // Recoge el objeto si el jugador está cerca
             RecogerObjeto();
+
         }
+
     }
 
     private void RecogerObjeto()
@@ -42,15 +45,20 @@ public class PickUpFlor : MonoBehaviour
         // Verifica si el jugador está cerca del objeto
         if (Vector2.Distance(transform.position, inventory.transform.position) < 1.5f)
         {
+
             // Itera a través de los espacios de inventario
             for (int i = 0; i < inventory.slotsFlor.Length; i++)
             {
+
                 // Verifica si el espacio de inventario está vacío
                 if (!inventory.isFull[i])
                 {
+
                     if (inventory.slotsFlor[i].transform.childCount > 0)
                     {
-                        Destroy(inventory.slotsFlor[i].transform.GetChild(0).gameObject);                        
+
+                        Destroy(inventory.slotsFlor[i].transform.GetChild(0).gameObject);     
+                        
                     }
 
                     // Marca el espacio de inventario como lleno
@@ -65,26 +73,28 @@ public class PickUpFlor : MonoBehaviour
                     {
                         Cflor = true;
                         Debug.Log("Es una flor especial");
-                        //Ghosty.SetActive(false);
-                        //Instantiate(GhostyOther, spawn.transform.position, Quaternion.identity, spawn.transform);
+
                     }
                     else
                     {
                         Cflor = false;
                         Debug.Log("Es una flor normal");
-                        Ghosty.SetActive(true);
-                        //GhostyOther.SetActive(false);
+
                     }
 
                     // Obtén el componente Button del nuevo objeto
                     Button newItemButton = newItem.GetComponent<Button>();
+
                     if (newItemButton != null)
                     {
                         // Agrega un listener al evento OnClick del botón
                         newItemButton.onClick.AddListener(() => {
+
                             foreach (Transform child in inventory.Panel.transform)
                             {
+
                                 child.gameObject.SetActive(false);
+
                             }
 
                             textomostrar.SetActive(true);
